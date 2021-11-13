e7219c99ab62707263badb7546a2a4724f8fc9d2
sushi30 <imri.paran@gmail.com>
2021-11-13 13:37:14 +0000

    changed to TS

--- bin/cli.js
+++ bin/cli.js
@@ -1,62 +1,51 @@
 #!/usr/bin/env node
-const cliPkg = require("../package");
-const commander = require("commander");
-const color = require("colorette");
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var createCommand = require("commander").createCommand;
+var colorette_1 = __importDefault(require("colorette"));
+var scrape_1 = require("./utils/scrape");
+var command = createCommand();
 function exit(text) {
-	if (text instanceof Error) {
-		if (text.message) {
-			console.error(color.red(text.message));
-		}
-		console.error(
-			color.red(`${text.detail ? `${text.detail}\n` : ""}${text.stack}`)
-		);
-	} else {
-		console.error(color.red(text));
-	}
-	process.exit(1);
+    if (text instanceof Error) {
+        if (text.message) {
+            console.error(colorette_1.default.red(text.message));
+        }
+        console.error(colorette_1.default.red("" + (text.name ? text.message + "\n" : "") + text.stack));
+    }
+    else {
+        console.error(colorette_1.default.red(text));
+    }
+    process.exit(1);
 }
-
 function success(text) {
-	console.log(text);
-	process.exit(0);
+    console.log(text);
+    process.exit(0);
 }
-
-async function scrape(opts) {}
-
 function invoke() {
-	let modulePackage = {};
-	try {
-		modulePackage = require(path.join(
-			path.dirname(env.modulePath),
-			"package.json"
-		));
-	} catch (e) {}
-
-	const cliVersion = [
-		color.blue("Knex CLI version:"),
-		color.green(cliPkg.version),
-	].join(" ");
-
-	const localVersion = [
-		color.blue("Knex Local version:"),
-		color.green(modulePackage.version || "None"),
-	].join(" ");
-
-	commander
-		.command("scrape")
-		.description("Scrape transactions.")
-		.option("--verbose", "verbose")
-		.option("--specific", "run specific seed file")
-		.action(() => {
-			scrape(commander.opts())
-				.then(() => {
-					success(color.green(`Scraped succesfully`));
-				})
-				.catch(exit);
-		});
-
-	commander.parse(process.argv);
+    var modulePackage = {};
+    var cliVersion = [
+        colorette_1.default.blue("Knex CLI version:"),
+        colorette_1.default.green("0"),
+    ].join(" ");
+    var localVersion = [
+        colorette_1.default.blue("Knex Local version:"),
+        colorette_1.default.green(modulePackage.version || "None"),
+    ].join(" ");
+    command
+        .command("scrape")
+        .description("Scrape transactions.")
+        .option("--verbose", "verbose")
+        .option("--specific", "run specific seed file")
+        .action(function () {
+        (0, scrape_1.scrape)(command.opts())
+            .then(function () {
+            success(colorette_1.default.green("Scraped succesfully"));
+        })
+            .catch(exit);
+    });
+    command.parse(process.argv);
 }
-
 invoke();
---
+++ src/cli.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+const { createCommand } = require("commander");
+import color from "colorette";
+import { scrape } from "./utils/scrape";
+
+const command = createCommand();
+
+function exit(text: any) {
+	if (text instanceof Error) {
+		if (text.message) {
+			console.error(color.red(text.message));
+		}
+		console.error(
+			color.red(`${text.name ? `${text.message}\n` : ""}${text.stack}`)
+		);
+	} else {
+		console.error(color.red(text));
+	}
+	process.exit(1);
+}
+
+function success(text: string) {
+	console.log(text);
+	process.exit(0);
+}
+
+function invoke() {
+	let modulePackage: any = {};
+
+	const cliVersion = [
+		color.blue("Knex CLI version:"),
+		color.green("0"),
+	].join(" ");
+
+	const localVersion = [
+		color.blue("Knex Local version:"),
+		color.green(modulePackage.version || "None"),
+	].join(" ");
+
+
+	command
+		.command("scrape")
+		.description("Scrape transactions.")
+		.option("--verbose", "verbose")
+		.option("--specific", "run specific seed file")
+		.action(() => {
+			scrape(command.opts())
+				.then(() => {
+					success(color.green(`Scraped succesfully`));
+				})
+				.catch(exit);
+		});
+
+	command.parse(process.argv);
+}
+
+invoke();
